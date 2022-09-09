4516e23009af1bd1a2daa44078be87d73dd0b3d1
Anny Domingos <annyds1@icloud.com>
2022-09-09 19:46:27 -0300

    ultimos ajustes tipo-movimentacao

--- financeiro/forms.py
+++ financeiro/forms.py
@@ -4,14 +4,14 @@ from .models import Movimentacao
 from django.utils import timezone
 
 class MovimentacaoForm(forms.Form):
-  # TIPO_DESPESA = 'DESPESA'
-  # TIPO_ENTRADA = 'ENTRADA'
+  TIPO_DESPESA = 'DESPESA'
+  TIPO_ENTRADA = 'ENTRADA'
 
-  # TIPO_MOVIMENTACAO_CHOICES = (
-  #   (TIPO_DESPESA, 'despesa'),
-  #   (TIPO_ENTRADA, 'entrada'),
-  # )
-  # tipo_movimentacao = widget=forms.Select(choices=TIPO_MOVIMENTACAO_CHOICES)
+  TIPO_MOVIMENTACAO_CHOICES = (
+    (TIPO_DESPESA, 'despesa'),
+    (TIPO_ENTRADA, 'entrada'),
+  )
+  tipo_movimentacao = forms.CharField(widget=forms.Select(choices=TIPO_MOVIMENTACAO_CHOICES))
   carteira = forms.IntegerField()
   usuario = forms.IntegerField()
   valor = forms.DecimalField(decimal_places=2, max_digits=10)
